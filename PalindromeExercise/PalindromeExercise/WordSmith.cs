c6a0be89fe5f62f0ac4dbac2a32f9d5e0840a607
Garrett Breaux <garrett.breaux1@gmail.com>
2021-08-16 17:17:06 -0500

    finished exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+        public bool IsAPalindrome(string word)
+        {
+
+            var reversed = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+
+            }
+            if (word == reversed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+            
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,22 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hey", false)]
+        [InlineData("nun", true)]
+        [InlineData("kayak", true)]
+        [InlineData("hair", false)]
+        public void IsAPalindrome(string word, bool expected)
         {
+            var test = new WordSmith();
+            bool actual = test.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
 
         }
     }
